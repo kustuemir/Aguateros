212b364be140df8e3896eb6664405518837e1a1b
kustuemir <salvadoremirgali@gmail.com>
2026-08-19 01:36:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 // CONFIGURACION DE AGUATERO V2
 // Completar con el NUEVO proyecto Supabase. Nunca uses la service_role key.
+
 window.AGUATERO_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://ipsijhgeyqoebzzcoxiy.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Xbh2cr3W6k_tfwF7DZ8NMw_bz5F2lOq',
   paymentsFunctionsUrl: '',
   supportWhatsapp: ''
 };
